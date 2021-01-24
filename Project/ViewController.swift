364ece2f807b9ccd3ba08811020962e8329833c2
Heri Susanto <herisusanto.id@gmail.com>
2021-01-24 11:24:21 +0700

    Initial Commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Heri Susanto on 24/01/21.
+//  Copyright © 2021 Heri Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
